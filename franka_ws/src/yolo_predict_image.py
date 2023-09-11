3a83cdf535543d0fa8c66dd709cb9876adc437db
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2023-09-11 23:27:28 +1000

    Able to access predicted class labels

--- franka_ws/src/yolo_predict_image.py
+++ franka_ws/src/yolo_predict_image.py
@@ -3,21 +3,32 @@ import cv2
 
 # Load pretrained YOLOv8 model
 model = YOLO('yolov8m.pt')
+class_names = model.names
 
 # Run batched inference on a list of images
 source_images = ['sample_image_1.jpeg', 'sample_image_2.jpeg']
 
 # Return a list of Results objects
-results = model(source=source_images[1], 
+results = model(source=source_images[0],
                 # conf=0.8,
-                show=True, 
-                save=True, 
+                show=True,
+                save=True,
                 boxes=True
-            )
+                )
 
 # Process results list
 for result in results:
     boxes = result.boxes  # Boxes object for bbox outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
     keypoints = result.keypoints  # Keypoints object for pose outputs
     probs = result.probs  # Probs object for classification outputs
+
+    con = result.boxes.conf
+    class_ids = result.boxes.cls
+    # x1 = int(result['xmin'])
+    # y1 = int(result['ymin'])
+    # x2 = int(result['xmax'])
+    # y2 = int(result['ymax'])
+
+    for class_id in class_ids:
+        print("Class:", class_names[int(class_id)])
+    print("\n-----------\n")
