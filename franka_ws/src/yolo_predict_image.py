318804444d59203d2065e412fed353720266abdd
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2023-09-11 14:24:46 +1000

    Test yolov8n model on object detection for images

---
+++ franka_ws/src/yolo_predict_image.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+import cv2
+
+# Load a model
+model = YOLO('yolov8n.pt')  # pretrained YOLOv8n model
+
+# Run batched inference on a list of images
+results = model(source='sample_image.jpeg', show=True, save=True, boxes=True)  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bbox outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
