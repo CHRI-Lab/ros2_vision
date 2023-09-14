c5487dccb9ff9d9d3062c33da9956c45369dbd1c
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2023-09-14 16:44:36 +1000

    Extract bounding box coordinates for streaming video

--- franka_ws/src/yolo_predict_streaming.py
+++ franka_ws/src/yolo_predict_streaming.py
@@ -4,6 +4,7 @@ import cv2
 # Load pretrained YOLOv8 model
 # The yolov8m model may be too computational intensive
 model = YOLO('yolov8n.pt')
+class_names = model.names
 
 # Define streaming source (e.g. camera dev number)
 # May need to try out for different ports
@@ -13,12 +14,28 @@ camera_dev = '1'
 results = model(source=camera_dev, 
                 show=True, 
                 # save=True, 
-                boxes=True
+                boxes=True,
+                stream=True
             )
 
 # Process results list
 for result in results:
-    boxes = result.boxes  # Boxes object for bbox outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
+    confidences = result.boxes.conf
+    class_ids = result.boxes.cls
+    box_coordinates = result.boxes.xyxy
+
+
+    print("=============\n")
+    for i in range(len(class_ids)):
+        class_id = class_ids[i]
+        coordinates = box_coordinates[i]
+
+        x1, y1 = float(coordinates[0]), float(coordinates[1])
+        x2, y2 = float(coordinates[2]), float(coordinates[3])
+
+        print("Class:", class_names[int(class_id)])
+        print("Confidence: {:.2f}".format(float(confidences[i])))
+        print("Top left coordinates: ({:.2f}, {:.2f})".format(x1, y1))
+        print("Bot right coordinates: ({:.2f}, {:.2f})".format(x2, y2))
+        
+        print("-----------\n")
