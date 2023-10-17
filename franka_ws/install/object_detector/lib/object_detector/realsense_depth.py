0fac4a64fca6334629e76feac4311f2a16f4f92d
Jackson Hu <renweih@student.unimelb.edu.au>
2023-10-18 02:30:51 +1100

    Resolve conflict when claiming the camera RGB endpoint

--- franka_ws/install/object_detector/lib/object_detector/realsense_depth.py
+++ franka_ws/install/object_detector/lib/object_detector/realsense_depth.py
@@ -15,7 +15,7 @@ class DepthCamera:
 
         # Stream at 30 fps
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        # config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
         # Start streaming
         self.pipeline.start(config)
@@ -23,17 +23,13 @@ class DepthCamera:
     def get_frame(self):
         frames = self.pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
-        # color_frame = frames.get_color_frame()
+        color_frame = frames.get_color_frame()
 
         depth_image = np.asanyarray(depth_frame.get_data())
-        # color_image = np.asanyarray(color_frame.get_data())
-        # if not depth_frame or not color_frame:
-        #     return False, None, None
-        # return True, depth_image, color_image
-
-        if not depth_frame:
-            return False, None
-        return True, depth_image
+        color_image = np.asanyarray(color_frame.get_data())
+        if not depth_frame or not color_frame:
+            return False, None, None
+        return True, depth_image, color_image
 
     def release(self):
         self.pipeline.stop()
